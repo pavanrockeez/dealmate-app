48ef841a32530e07b5dba27c4ea06c499b242620
Pavan Rayipudi <pavanrayipudi111@gmail.com>
2023-05-23 13:17:36 -0700

    add deals positions

--- src/components/home.js
+++ src/components/home.js
@@ -2,18 +2,20 @@ import React, { useState } from 'react'
 import '../assets/css/styles.scss';
 import dealmate_icon from '../assets/img/dealmate_icon.svg';
 import google_paly from '../assets/img/google_paly.svg';
-import play_store from '../assets/img/play_store.svg';
-import Products from './products';
+import OwlCarousel from "react-owl-carousel";
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
 import TopBrands from './topBrands';
-import apple_store from '../assets/img/apple_store.svg';
+import share_icon from '../assets/img/share_icon.svg';
 import Offers from './offers';
 import ForKids from './forKids';
 import Footer from './footer';
-import offers_icon from '../assets/img/offers_icon.svg';
 import './footer.scss';
 import { useNavigate } from 'react-router-dom';
 import Mobiles from './mobiles';
 import GreatDeals from './greatDeals';
+import './deals.scss'
+import img1 from '../assets/img/puma_brand.svg';
 const Home = () => {
   const [navTabs, setNavTabs] = useState('men');
   const navigate = useNavigate();
@@ -38,6 +40,71 @@ const Home = () => {
     setNavTabs(tab);
     navigate('/alldeals');
   }
+  const products = [
+    {
+      id: 1,
+      img: img1,
+      rating: 4.5,
+      viewers: 135,
+      mrp: 112,
+      offer: 20,
+      percentate: '20%',
+      brnadlogo: ''
+    },
+    {
+      id: 2,
+      img: img1,
+      rating: 4.5,
+      viewers: 135,
+      mrp: 112,
+      offer: 20,
+      percentate: '20%',
+      brnadlogo: ''
+    },
+    {
+      id: 3,
+      img: img1,
+      rating: 4.5,
+      viewers: 135,
+      mrp: 112,
+      offer: 20,
+      percentate: '20%',
+      brnadlogo: ''
+    },
+    {
+      id: 4,
+      img: img1,
+      rating: 4.5,
+      viewers: 135,
+      mrp: 112,
+      offer: 20,
+      percentate: '20%',
+      brnadlogo: ''
+    },
+    {
+      id: 5,
+      img: img1,
+      rating: 4.5,
+      viewers: 135,
+      mrp: 112,
+      offer: 20,
+      percentate: '20%',
+      brnadlogo: ''
+    },
+    {
+      id: 6,
+      img: img1,
+      rating: 4.5,
+      viewers: 135,
+      mrp: 112,
+      offer: 20,
+      percentate: '20%',
+      brnadlogo: ''
+    }
+  ];
+  const navigateToAllDeals =()=>{
+    navigate('/alldeals')
+  }
   return (
     <div className='home_page' style={{ overflowY: 'scroll', height: '100vh' }}>
       <div className='header_div'>
@@ -61,7 +128,7 @@ const Home = () => {
             <p>Lorem ipsum is placeholder text commonly used in the graphic,</p>
           </div>
         </div>
-        <div className='row'>
+        <div className='row navtabs-show'>
           <div className='col-lg-2'></div>
           <div className='col-lg-8 justify-content-center mb-3'>
             <div className="bloc-tab">
@@ -95,6 +162,61 @@ const Home = () => {
           <div className='col-lg-2'></div>
         </div>
       </div>
+      <div className='row dealers'>
+        <div className='col-lg-2'></div>
+        <div className='col-lg-9'>
+          <div className='row alldeals justify-content-center'>
+            {products.map((item, i) => (
+              <div className="col-lg-3 card_dtl mr-2" key={i}>
+                <div className="card">
+                  <div className="images_sec">
+                    <div className='text-left ratings'>
+                      <h5 className="card_head">{item.rating}</h5>
+                      <p className="grey_para">{item.viewers}</p>
+                    </div>
+                    <OwlCarousel
+                      items={1}
+                      className="owl-theme property_car"
+                      margin={20}
+                      loop
+                      dots={true}
+                    >
+                      <div className="item">
+                        <img src={item.img} className="main_img" alt="image" />
+                      </div>
+                      <div className="item">
+                        <img src={item.img} className="main_img" alt="image" />
+                      </div>
+                      <div className="item">
+                        <img src={item.img} className="main_img" alt="image" />
+                      </div>
+                    </OwlCarousel>
+                  </div>
+                  <div className="card_body text-center">
+                    <div className="d-flex justify-content-center card_b_details">
+                      <div className='text-center'>
+                        <h5>{item.percentate} OFF</h5>
+                        <p className="body_para text-left">MRP ₹{item.mrp} |  Offer ₹{item.offer} </p>
+                      </div>
+
+                    </div>
+                    <div className='hr_line'></div>
+                    <div className='d-flex justify-content-between shop_now'>
+                      <img src={share_icon} alt="share_icon" />
+                      <button>Shop Now</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+            <div className='col-lg-3'></div>
+          </div>
+        </div>
+        <div className='col-lg-1'></div>
+      </div>
+      <div className='row justify-content-center load_more'>
+        <button onClick={()=> navigateToAllDeals()}>SEE ALL</button>
+      </div>
       <TopBrands />
       <Offers />
       <ForKids />
@@ -105,7 +227,7 @@ const Home = () => {
           <Footer />
         </div>
       </div>
-    </div>
+    </div >
   )
 }
 
